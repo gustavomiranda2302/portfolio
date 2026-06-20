fa22d5e18cc2e45a193b12fbfc07c0dae6d073e5
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-19 22:35:23 -0300

    feat: ExternalButton.tsx (botao para o e-mail, github e linkedIn

---
+++ src/components/ExternalButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/ExternalButton.css"
+interface ExternalButtonProps {
+    href: string;
+    svgUrl: string;
+    label: string;
+
+}
+
+export const ExternalButton: React.FC<ExternalButtonProps> = ({ href, svgUrl, label }) => {
+    return (
+        <a href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="external-button"
+        >
+            <img src={svgUrl} alt="icone do ${label}" />
+            {label}
+        </a>
+    )
+}

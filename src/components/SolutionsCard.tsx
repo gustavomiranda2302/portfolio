55fff9e8129bc1672736abbe2262920eda0ca8ca
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-23 10:15:25 -0300

    fix: ajustes no css mobile

--- src/components/SolutionsCard.tsx
+++ src/components/SolutionsCard.tsx
@@ -6,13 +6,12 @@ interface SolutionsCardProps {
     alt?: string,
     title: string,
     text: string;
-
-
 }
+
 export default function SolutionCard({ image, alt, title, text }: SolutionsCardProps) {
     return (
         <div className="card">
-            <img src={image} alt={alt && title} />
+            <img src={image} alt={alt ?? title} />
             <h3>{title}</h3>
             <p>{text}</p>
         </div>
